60019a1821b5b8fc9ce8d2be9c810b8c529c5a20
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-07-20 09:31:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const hqBlock = 42
+const blockLengthInFeet = 264
+const pricePerFootInCents = 2
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(hqBlock - block)
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * blockLengthInFeet
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return Math.abs(
+    (block1 - block2) * blockLengthInFeet
+  )
+}
+
+function calculatesFarePrice(block1, block2) {
+  let fareDistance = distanceTravelledInFeet(block1, block2)
+  if (fareDistance <= 400) {
+    return 0
+  } else if (fareDistance > 400 && fareDistance <= 2000) {
+    return ((fareDistance - 400) * pricePerFootInCents) / 100
+  } else if (fareDistance > 2000 && fareDistance < 2500){
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
